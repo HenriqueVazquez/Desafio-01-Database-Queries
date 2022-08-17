88c83934bd3d3f9ade7532c480b4b567a5c5e8b6
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2022-08-17 19:18:39 -0300

    :fire:

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,24 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    .createQueryBuilder()
+    .select("games")
+    .from(Game, "games")
+    .where("games.title  ILIKE :title", { title: `%${param}%` })
+    .getMany();    
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(*) FROM games;`); 
   }
 
-  async findUsersByGameId(id: string): Promise<User[]> {
+  async findUsersByGameId(id: string): Promise<User[]> {   
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    .createQueryBuilder()
+    .select("users")
+    .from(User, "users")
+    .leftJoin("users.games", "games")
+    .where("games.id = :id", { id })
+    .getMany();     
   }
 }
